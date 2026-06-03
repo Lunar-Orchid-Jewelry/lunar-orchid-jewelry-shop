3fd172e132c59aebed6dfc5518705c6fa9814629
Robin <rlizmosher15@gmail.com>
2026-06-03 17:10:21 -0400

    6/3 update3

--- src/Home.tsx
+++ src/Home.tsx
@@ -104,28 +104,10 @@ function Home() {
                   name: "Cosmic Oasis",
                   price: "$PRICE",
                 },
-              ].map((item, i) => (
-                <div key={i} className=" rounded-xl hover:shadow-lg ">
-                  <a
-                    href="bello-opal-sterling-silver-galaxy-pendant.html"
-                    className="block"
-                  >
-                    <div className="overflow-hidden hover:scale-98  transition rounded-sm mb-3">
-                      <img
-                        src={item.img}
-                        alt="Sterling Silver Bello Opal Necklace Pendant"
-                        className="aspect-square w-full h-auto object-cover"
-                      />
-                    </div>
-                    <h4 className="text-secondary text-base font-josefin text-center mb-1">
-                      {item.name}
-                    </h4>
-                    <p className="text-dark text-lg font-josefin text-center">
-                      {item.price}
-                    </p>
-                  </a>
-                </div>
-              ))}
+              ].map((item, ix) => {
+                const it = { ix, ...item };
+                return <ProductTile item={it} />;
+              })}
             </div>
           </div>
           <div className="text-center mt-8">
@@ -757,5 +739,41 @@ function Home() {
     </>
   );
 }
+
+type ProductTileProps = {
+  item: ProductTileItem;
+};
+type ProductTileItem = {
+  ix: number;
+  img: string;
+  name: string;
+  price: string;
+};
+const ProductTile = ({ item }: ProductTileProps) => {
+  return (
+    <>
+      <div key={item.ix} className=" rounded-xl hover:shadow-lg ">
+        <a
+          href="bello-opal-sterling-silver-galaxy-pendant.html"
+          className="block"
+        >
+          <div className="overflow-hidden hover:scale-98  transition rounded-sm mb-3">
+            <img
+              src={item.img}
+              alt="Sterling Silver Bello Opal Necklace Pendant"
+              className="aspect-square w-full h-auto object-cover"
+            />
+          </div>
+          <h4 className="text-white text-base font-josefin text-center mb-2">
+            {item.name}
+          </h4>
+          <p className="text-dark text-lg font-josefin text-center">
+            {item.price}
+          </p>
+        </a>
+      </div>
+    </>
+  );
+};
 
 export default Home;
